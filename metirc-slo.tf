f946f2098c49d180f17cd5aba6b729a0ea0f63a8
laptop <priorlewis43@gmail.com>
2023-11-07 20:27:29 -0600

    updates

---
+++ metirc-slo.tf
@@ -0,0 +1,29 @@
+# Metric-Based SLO
+# Create a new Datadog service level objective
+resource "datadog_service_level_objective" "foo" {
+  name        = "Example Metric SLO"
+  type        = "metric"
+  description = "My custom metric SLO"
+  query {
+    numerator   = "sum:my.custom.count.metric{type:good_events}.as_count()"
+    denominator = "sum:my.custom.count.metric{*}.as_count()"
+  }
+
+  thresholds {
+    timeframe = "7d"
+    target    = 99.9
+    warning   = 99.99
+  }
+
+  thresholds {
+    timeframe = "30d"
+    target    = 99.9
+    warning   = 99.99
+  }
+
+  timeframe         = "30d"
+  target_threshold  = 99.9
+  warning_threshold = 99.99
+
+  tags = ["foo:bar", "baz"]
+}
